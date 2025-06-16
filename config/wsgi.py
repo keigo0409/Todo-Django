85dbb05eee19ed63fcb16484b7020ff9126f1c9a
kamata keigo <k.kamata.sys23@morijyobi.ac.jp>
2025-06-16 19:29:07 +0900

    deploy

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,7 +1,17 @@
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+django.setup()
+
+from django.core.management import call_command
+from django.db import OperationalError
+
+try:
+    call_command('migrate', interactive=False)
+except OperationalError as e:
+    print("Database not ready yet:", e)
